2f56fc100be901d691c73429a582b2fad24cb686
smart2004 <115572648+smart2004@users.noreply.github.com>
2023-03-20 22:29:51 +0200

    Update_March20

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -14,5 +14,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     message = 'It is prohibitted to change foreign content!'
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        return(
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
